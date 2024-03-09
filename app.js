1aa5c0a8734652318bf117944b53fefc370c3ea5
Ben Sloan <mrbensloan@gmail.com>
2024-03-09 14:16:03 +0000

    Added 2 basic yapily endpoints

--- app.js
+++ app.js
@@ -1,27 +1,71 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/dist', express.static(__dirname + '/dist'))
+app.use("/dist", express.static(__dirname + "/dist"));
 app.use(express.urlencoded());
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        title: 'Home'
-    })
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Home",
+  });
+});
 
+app.get("/institutions", async (req, res) => {
+  const institutionsConfig = {
+    method: "get",
+    url: "https://api.yapily.com/institutions",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          "aeb4b47e-db4c-4adc-b23a-da1125ea99af:AXGJbNpY2mxEvIMB1pIlpu5RoONliMOS"
+        ).toString("base64"),
+    },
+  };
+  const data = await axios(institutionsConfig);
+  res.json(data.data);
+});
 
+app.post("/auth", async (req, res) => {
+  const { userID, institutionID } = req.body;
+  const query = new URLSearchParams({
+    raw: "true",
+  }).toString();
+
+  const authConfig = {
+    method: "post",
+    url: `https://api.yapily.com/account-auth-requests?${query}`,
+    data: JSON.stringify({
+      applicationUserId: userID,
+      institutionId: institutionID,
+      callback: "/",
+    }),
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          "aeb4b47e-db4c-4adc-b23a-da1125ea99af:AXGJbNpY2mxEvIMB1pIlpu5RoONliMOS"
+        ).toString("base64"),
+    },
+  };
+
+  const data = await axios(authConfig);
+  res.json(data.data);
+});
 
 app.use((req, res) => {
-    res.status(404);
-    res.render('404', {
-        title: '404'
-    })
-})
+  res.status(404);
+  res.render("404", {
+    title: "404",
+  });
+});
